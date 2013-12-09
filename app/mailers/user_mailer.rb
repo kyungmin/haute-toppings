2e08ddd9924b3832d5194ea592c5b5d391c8ef10
Kyungmin Kim <kmin905@gmail.com>
2013-12-08 16:11:39 -0800

    Update user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,22 +3,22 @@ class UserMailer < ActionMailer::Base
   default from: @admin_email
 
   def customer_confirmation_email(customer)
-    @customer_id = customer.id
-    @customer_email = customer.email
-    @start_date = customer.start_date
-    @interval = customer.interval
-    @amount = customer.amount
+    @customer_id = customer["id"]
+    @customer_email = customer["email"]
+    @start_date = customer["start_date"]
+    @interval = customer["interval"]
+    @amount = customer["amount"]
 
     mail(to: @customer_email, subject: "Haute Toppings Membership Confirmation")
   end
 
   def admin_notification_email(customer, customer_name, customer_email)
-    @customer_id = customer.id
-    @customer_name = customer.name
-    @customer_email = customer.email
-    @interval = customer.interval
+    @customer_id = customer["id"]
+    @customer_name = customer["name"]
+    @customer_email = customer["email"]
+    @interval = customer["interval"]
     @admin_internal_email = ENV['ADMIN_INTERNAL_EMAIL']
-    
+
     mail(to: @admin_internal_email, subject: "NEW MEMBER: " + @customer_name)
   end
 end
